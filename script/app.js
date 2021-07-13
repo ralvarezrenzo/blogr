0e8d0e0f424d218d5a482d0c9798ee42ab50a2e9
renzo <ralvarezrenzo@gmail.com>
2021-07-13 15:50:00 -0500

    arrowNavPrueba

--- script/app.js
+++ script/app.js
@@ -1,7 +1,8 @@
 import navBar from "./events/navbar.js";
 
 const d = document;
+const w = window;
 
 d.addEventListener('DOMContentLoaded',(e)=>{
-    navBar('.a-menu','#button-menu','.menu-box','.arrow');
+    navBar('.a-menu','#button-menu','.menu-box','.flecha-change');
 })
--- script/events/navbar.js
+++ script/events/navbar.js
@@ -1,5 +1,6 @@
-export default function navBar(aMenu, buttonMenu, menuBox,arrowChange){
+export default function navBar(aMenu, buttonMenu, menuBox,flechaChange){
     const d = document;
+    const w = window;
         let aMenuBtn= d.querySelectorAll(aMenu)
             aMenuBtn.forEach(aMenuClick =>{
                 aMenuClick.addEventListener('click',(e)=>{
@@ -21,4 +22,32 @@ export default function navBar(aMenu, buttonMenu, menuBox,arrowChange){
             menuBoxAll.classList.toggle('menu-box--open')
         }
     })
+    let flecha = d.querySelectorAll(flechaChange);
+        let flechaA = Array.from(flecha)
+        if(innerWidth >  991){
+        console.log(flechaA)
+        flechaA.forEach(flechas=>{
+            flechas.setAttribute('stroke','#fff')
+        })
+        }else{
+            flechaA.forEach(flechas=>{
+                flechas.setAttribute('stroke','#FF7B86')
+            })
+        }
+    w.addEventListener('resize',(e)=>{
+        console.log(innerWidth)
+        let flecha = d.querySelectorAll(flechaChange);
+        let flechaA = Array.from(flecha)
+        if(innerWidth >  991){
+        console.log(flechaA)
+        flechaA.forEach(flechas=>{
+            flechas.setAttribute('stroke','#fff')
+        })
+        }else{
+            flechaA.forEach(flechas=>{
+                flechas.setAttribute('stroke','#FF7B86')
+            })
+        }
+    })
+    
 }
